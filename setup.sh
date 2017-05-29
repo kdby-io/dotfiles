10c0f255dd648b0219926fe5a867dc4e7a831e24
pueue <pueuepueue@gmail.com>
2017-05-30 07:27:23 +0900

    Change to symbolic link

--- setup.sh
+++ setup.sh
@@ -4,18 +4,18 @@
 # Copy config files
 
 ## Oh my zsh
-ln -f .zshrc ~/.zshrc
+ln -sf .zshrc ~/.zshrc
 
 ## Tmux
-ln -f .tmux.conf ~/.tmux.conf
+ln -sf .tmux.conf ~/.tmux.conf
 
 ## Neovim
 mkdir -p ~/.config/nvim
-ln -f init.vim ~/.config/nvim/init.vim
+ln -sf init.vim ~/.config/nvim/init.vim
 
 ## Tmuxp
 mkdir -p ~/.tmuxp
-ln -f tumblbug-backend.yaml ~/.tmuxp/tumblbug-backend.yaml # tumblbug backend
+ln -sf tumblbug-backend.yaml ~/.tmuxp/tumblbug-backend.yaml # tumblbug backend
 
 
 # Restart
