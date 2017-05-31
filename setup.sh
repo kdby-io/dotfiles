84e5dc691915457742608d67b0b951e4e3854819
pueue <pueuepueue@gmail.com>
2017-05-31 14:28:19 +0900

    Fix setup script by using absolute path

--- setup.sh
+++ setup.sh
@@ -1,21 +1,22 @@
 #!/bin/zsh
 
+PWD=`pwd`
 
 # Copy config files
 
 ## Oh my zsh
-ln -sf .zshrc ~/.zshrc
+ln -sf $PWD/.zshrc ~/.zshrc
 
 ## Tmux
-ln -sf .tmux.conf ~/.tmux.conf
+ln -sf $PWD/.tmux.conf ~/.tmux.conf
 
 ## Neovim
 mkdir -p ~/.config/nvim
-ln -sf init.vim ~/.config/nvim/init.vim
+ln -sf $PWD/init.vim ~/.config/nvim/init.vim
 
 ## Tmuxp
 mkdir -p ~/.tmuxp
-ln -sf tumblbug-backend.yaml ~/.tmuxp/tumblbug-backend.yaml # tumblbug backend
+ln -sf $PWD/tumblbug-backend.yaml ~/.tmuxp/tumblbug-backend.yaml # tumblbug backend
 
 
 # Restart
