0e9eb1bc9a769728c5965c2ed47cb56331622d64
Sangwon Kim <pueuepueue@gmail.com>
2019-06-16 22:11:16 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -1,22 +1,55 @@
 #!/bin/sh
 
+PWD=`pwd`
+
+echo "$(tput setaf 2)Greetings.$(tput sgr 0)"
+
 #--------------- Homebrew ---------------
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew="/usr/local/bin/brew"
+if [ -f "$brew" ]
+then
+  echo "$(tput setaf 2)Homebrew is already installed.$(tput sgr 0)"
+else
+  echo "$(tput setaf 3)Installing Homebrew. Homebrew requires osx command lines tools, please download xcode first$(tput sgr 0)"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 
 #----------------- tmux -----------------
 brew install tmux
 git clone https://github.com/tmux-plugins/tpm ./.tmux/plugins/tpm
-ln -sf ./.tmux.conf ~/.tmux.conf
+ln -sf $PWD/.tmux.conf ~/.tmux.conf
 
+
+#----------------- zsh ------------------
+brew install zsh
+
+#-=------------- thefuck ----------------
+brew install thefuck
+
+#---------------- neovim ----------------
+brew install neovim
+mkdir -p ~/.config/nvim
+ln -sf $PWD/init.vim ~/.config/nvim/init.vim
+
+#----------------- lsd ------------------
+brew install lsd
+
+#------------------ z -------------------
+brew install z
 
 #-------------- oh-my-zsh ---------------
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ln -sf ./.zshrc ~/.zshrc
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "$(tput setaf 3)Installing oh-my-zsh.$(tput sgr 0)"
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+  echo "$(tput setaf 2)oh-my-zsh already installed.$(tput sgr 0)"
+fi
+ln -sf $PWD/.zshrc ~/.zshrc
 
 
 # spaceship-prompt
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/themes/spaceship-prompt
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 # zsh-completions
@@ -28,25 +61,13 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-
-#-=------------- thefuck ----------------
-brew install thefuck
-
-
-#---------------- neovim ----------------
-brew install neovim
-mkdir -p ~/.config/nvim
-ln -sf ./init.vim ~/.config/nvim/init.vim
-
-
-#----------------- lsd ------------------
-brew install lsd
-
-
-#----------------- zsh ------------------
-brew install zsh
-
-brew install z
+chsh -s $(which zsh)
 
 source ~/.zshrc
-echo "Done! Reopen a terminal";
+
+
+#---------------- hyper -----------------
+brew update
+brew cask install hyper
+
+echo "$(tput setaf 2)Enjoy :)$(tput sgr 0)"
