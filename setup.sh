421a21595bec0235762df55f8983f3a46922b051
scon-io <iam@scon.io>
2021-10-24 14:44:24 +0900

    Change shell

--- setup.sh
+++ setup.sh
@@ -11,36 +11,33 @@ then
   echo "$(tput setaf 2)Homebrew is already installed.$(tput sgr 0)"
 else
   echo "$(tput setaf 3)Installing Homebrew. Homebrew requires osx command lines tools, please download xcode first$(tput sgr 0)"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
+#
+# Oh my zsh
+#
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#----------------- tmux -----------------
-brew install tmux
-git clone https://github.com/tmux-plugins/tpm ./.tmux/plugins/tpm
-ln -sf $PWD/.tmux.conf ~/.tmux.conf
+# zsh theme
+brew install pure
+echo -e "\nautoload -U promptinit; promptinit" >> ~/.zshrc
+echo -e "\nprompt pure" >> ~/.zshrc
 
+# ----------------- tmux -----------------
+# brew install tmux
+# git clone https://github.com/tmux-plugins/tpm ./.tmux/plugins/tpm
+# ln -sf $PWD/.tmux.conf ~/.tmux.conf
 
-#----------------- fish ------------------
-brew install fish
-curl -L https://get.oh-my.fish | fish
-omf install bass
-
-#-------------- fish theme ---------------
-omf install pure
-chsh -s $(which fish)
-# https://github.com/rafaelrinaldi/pure/wiki/Oh-My-Fish-not-currently-supporting-conf.d-snippets-in-plugins-and-themes
-ln -s $OMF_PATH/themes/pure/conf.d/pure.fish ~/.config/fish/conf.d/pure.fish
-ln -s $OMF_PATH/themes/pure/conf.d/_pure_init.fish ~/.config/fish/conf.d/pure_init.fish
-ln -sf $PWD/fish/config.fish ~/.config/fish/config.fish
-ln -sf $PWD/fish/functions/fuck.fish ~/.config/fish/functions/fuck.fish
-ln -sf $PWD/fish/functions/nvm.fish ~/.config/fish/functions/nvm.fish
-omf theme pure
-omf install pj
-omf install z
 
 #--------------- thefuck ----------------
 brew install thefuck
+eval $(thefuck --alias)
+
+#--------------- asdf.vm ----------------
+brew install asdf
+echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ~/.zshrc
+echo -e "\n. $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash" >> ~/.zshrc
 
 #---------------- yarn ------------------
 brew install yarn
@@ -53,5 +50,3 @@ ln -sf $PWD/init.vim ~/.config/nvim/init.vim
 #----------------- lsd ------------------
 brew install lsd
 
-#----------------- hub ------------------
-brew install hub
